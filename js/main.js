20506adacc267947919df2a31b100e135323d986
Aleksandar Zedi <aca985@gmail.com>
2016-05-30 21:42:47 +0200

    setup, creating tree w/ assets, imports, base etc.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+
+  "use strict";
+  
+  /**
+   * Global variables available for all scopes
+   */
+
+  /**
+   * All function calls are placed on top for ease of access
+   * and personal coding practices.
+   */
+  $(document).ready(function(){
+
+  });
+  
+  
+  /**
+   * Call all the elements that need to be responsive
+   */
+  $(window).resize(function() {
+
+  });
+  
+  
+  // End of file
+})( jQuery, window, document );
