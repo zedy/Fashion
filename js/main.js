79b629564965a9b6449d21d79c20db7569cf30c8
Aleksandar Zedi <aca985@gmail.com>
2016-06-01 19:51:37 +0200

    slider functionallity

--- js/main.js
+++ js/main.js
@@ -7,17 +7,16 @@
   /**
    * Global variables available for all scopes
    */
-
+  var $prev = $('#slider').find('.left');
+  var $next = $('#slider').find('.right');  
+  
   /**
    * All function calls are placed on top for ease of access
    * and personal coding practices.
    */
   $(document).ready(function(){
     
-    $('#slider .right').click(function(){
-      var x = $('#slider .active').width();
-      $('#slider .active').animate({'left': -x});
-    });
+    main_slider_controlls();
     
   });
   
@@ -28,6 +27,50 @@
   $(window).resize(function() {
 
   });
+  
+  /**
+   * Logic behind the slider controlls
+   */
+  function main_slider_controlls() {
+    
+    // show previous slide        
+    $prev.click(function(e){
+      var $active = $('#slider').find('.active');
+      $active.fadeOut(150).removeClass('active');
+      
+      if ($active.hasClass('first')) {
+        $('#slider .slides').find('.last').fadeIn(150).addClass('active');
+      } else {
+        $active.prev().fadeIn(150).addClass('active');
+      }
+      
+    });
+    
+    // show next slide
+    $next.click(function(e){
+      var $active = $('#slider').find('.active');      
+      $active.fadeOut(150).removeClass('active');
+      
+      if ($active.hasClass('last')) {
+        $('#slider .slides').find('.first').fadeIn(150).addClass('active');
+      } else {
+        $active.next().fadeIn(150).addClass('active');
+      }
+      
+    });
+    
+    main_slider_nav_controll();
+    
+  }
+  
+  /**
+   * Logic behind the navs on the slider
+   * Is called when the event for moving
+   * a slide is fired.
+   */
+  function main_slider_nav_controll() {
+    
+  }
   
   
   // End of file
